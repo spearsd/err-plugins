15c222afe834eac26ffaf507cbc1c2d9214f559f
Dustin <dustin.spears@live.com>
2018-02-13 11:35:08 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,32 @@
+from errbot import BotPlugin, botcmd
+import subprocess, tempfile, re, time
+
+class AutoSysServer(BotPlugin):
+    """AutoSys server plugin for Errbot"""
+
+    @botcmd
+    def server_target(self, msg, args):
+        """Target server for jobs"""
+        self['target_server'] = args
+        self.target_server = args
+        #with open('/var/errbot/target_server', 'w+') as file:
+        #    proc = subprocess.Popen(['echo',args], stdout=file)
+        #    proc.wait()
+        #    file.seek(0)
+        #    target_server = str(target_server) + str(file.read())
+        return "Targeted server: " + self['target_server']
+    
+    @botcmd
+    def server_active(self, msg, args):
+        """Retrieve targeted server"""
+        #target_server = ""
+        #with open('/var/errbot/target_server', 'r') as file:
+        #    target_server = str(file.read())
+        return "Currently targeted server: " + self['target_server']
+    
+# Used to run commands in terminal and capture the result in string var.
+#with tempfile.TemporaryFile() as tempf:
+#    proc = subprocess.Popen(['ls','-l'], stdout=tempf)
+#    proc.wait()
+#    tempf.seek(0)
+#    string = str(string) + str(tempf.read())
