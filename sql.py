4ffffec4e7eb08f3f0581085c6404a596a6f8be4
Dustin <dustin.spears@live.com>
2018-02-15 10:51:01 -0500

    Update sql.py

--- sql.py
+++ sql.py
@@ -88,6 +88,7 @@ class SQLPlugin(BotPlugin):
         if error == "":
             sql_file_command = "mysql -u " + self.user + " " + self.passwd + " -h " + self.server + " < " + "/tmp/sql_file.sql"
             proc = subprocess.run([sql_file_command], shell=True, stdout=subprocess.PIPE)
+            yield proc
             outs, errs = proc.communicate()
             #output = subprocess.check_output(["mysql", "-u", self.user, self.passwd, "-h", self.server, "<", "/tmp/sql_file.sql"])
             yield str(outs)
