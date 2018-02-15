846f3946f543eeac9cf319a70196f37de6435a08
Dustin <dustin.spears@live.com>
2018-02-15 14:55:31 -0500

    Update sql.py

--- sql.py
+++ sql.py
@@ -74,7 +74,7 @@ class SQLPlugin(BotPlugin):
     def sql_file_retrieve(self, msg, args):
         """Retrieve file from errbot"""
         file_to_get = args[0]
-        string = '<a href="file:///tmp/' + file_to_get + '">' + file_to_get + '</a>'
+        string = '<a href="file:///media/sf_sql_files/' + file_to_get + '">' + file_to_get + '</a>'
         yield string
                 
     @botcmd(split_args_with=None)
