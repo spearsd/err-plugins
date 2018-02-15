7a0d5dedc87b3934b0e7343e383023ef69eced5e
Dustin <dustin.spears@live.com>
2018-02-15 10:28:44 -0500

    Update sql.py

--- sql.py
+++ sql.py
@@ -86,7 +86,7 @@ class SQLPlugin(BotPlugin):
         except:
             error = error + ""
             
-        if outs.upper().find("COMMIT;"):
+        if str(outs).upper().find("COMMIT;"):
             error = error + "COMMIT found in sql file, please remove this and try again. "
       # else:
            # These 2 lines ensure the sql file doesn't make actual changes to the db.
